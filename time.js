1ac2d455907b86a11a067af1c4572863cca186d0
gyheo <heo.geoyeo@gmail.com>
2020-03-03 23:59:23 +0900

    Calculate the days of life

--- time.js
+++ time.js
@@ -1,5 +1,6 @@
 const timeContainer = document.querySelector('.time-container');
 const clock = timeContainer.querySelector('h1');
+const intro = timeContainer.querySelector('p');
 
 function getTime() {
     const date = new Date();
@@ -11,6 +12,17 @@ function getTime() {
 
 function init() {
     getTime();
+
+    const today = new Date();
+    let birthYear = 1993;
+    let birthMonth = 1;
+    let birthDay = 14;
+
+    const dateObj = new Date(birthYear, birthMonth, birthDay);
+
+    const lifeTime = parseInt((today.getTime() - dateObj.getTime()) / 1000 / 60 / 60 / 24);
+    intro.innerText = `내가 살아온 일 수 : ${lifeTime}`;
+
     setInterval(getTime, 1000);
 }
 
